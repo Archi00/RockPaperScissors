b69254450e7424fc19b69964d359e629c1be5926
Archi00 <albert.archi@gmail.com>
2019-10-30 22:23:34 +0100

    Added results in DOM and gives results after 5th game

--- script.js
+++ script.js
@@ -3,22 +3,17 @@ let game_count = 0;
 let player_win_count = 0
 let computer_win_count = 0
 
-
 document.getElementById("rock").onclick = user;
 document.getElementById("paper").onclick = user;
 document.getElementById("scissors").onclick = user;
 
 function user(){
-    var playerPlay = this.id;
-    console.log("User: " + playerPlay)
+    let playerPlay = this.id;
+    console.log("You play: " + playerPlay + "!")
 
-
-let computerPlay = options[Math.floor((Math.random()*options.length))];
-console.log("Computer plays: " + computerPlay + "!");
-
-
-
-
+    let computerPlay = options[Math.floor((Math.random()*options.length))];
+    console.log("Computer plays: " + computerPlay + "!");
+    
 function game(computerPlay, playerPlay) {
     if (computerPlay == "rock" && playerPlay == "rock") {
       return ("Draw!!");
@@ -49,10 +44,7 @@ function game(computerPlay, playerPlay) {
     }
 }
 console.log(game(computerPlay, playerPlay));
-
 game_count++;
-
-}
 
 function game_result(player_win_count, computer_win_count) {
     if (player_win_count > computer_win_count) {
@@ -65,5 +57,23 @@ function game_result(player_win_count, computer_win_count) {
         return ("Player score = " + player_win_count + " Computer score = "
         + computer_win_count + " It's a Draw!!");
     }
+}
+const results_div = document.querySelector('#results');
+
+const player_content = document.createElement('div');
+player_content.textContent = ("You play: " + playerPlay + "!");
+results_div.appendChild(player_content);
+const comp_content = document.createElement('div');
+comp_content.textContent = ("Computer plays: " + computerPlay + "!");
+results_div.appendChild(comp_content);
+const result_content = document.createElement('div');
+result_content.textContent = (game(computerPlay, playerPlay));
+results_div.appendChild(result_content);
+
+if (game_count == 5){
+const gameResult = document.createElement('div');
+gameResult.textContent = (game_result(player_win_count, computer_win_count));
+results_div.appendChild(gameResult);
+}
 }
 
